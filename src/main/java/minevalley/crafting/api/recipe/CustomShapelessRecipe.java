d74c868194dcde6d0bd6ea8cf8457fae33db11fb
Thorben B. <thorbenbunger@gmail.com>
2026-02-11 19:53:46 +0100

    feat(CustomShapelessRecipe): implement ingredient adding

--- src/main/java/minevalley/crafting/api/recipe/CustomShapelessRecipe.java
+++ src/main/java/minevalley/crafting/api/recipe/CustomShapelessRecipe.java
@@ -1,6 +1,18 @@
 package minevalley.crafting.api.recipe;
 
+import minevalley.crafting.api.ingredient.RecipeIngredient;
+
+import javax.annotation.Nonnull;
+
 @SuppressWarnings("unused")
 public interface CustomShapelessRecipe extends CustomRecipe {
 
+    /**
+     * Adds an ingredient to the recipe.
+     *
+     * @param ingredient the ingredient to add
+     * @return this
+     * @throws IllegalArgumentException if the ingredient is null
+     */
+    CustomShapelessRecipe addIngredient(@Nonnull RecipeIngredient ingredient) throws IllegalArgumentException;
 }
